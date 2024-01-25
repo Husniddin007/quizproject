2fd06bb23923ff6a3b56ba3ef506c4d781cc72eb
Husniddin007 <husniddingofforov99@gmail.com>
2024-01-25 19:24:09 +0500

    postgres123456

--- QuizProject/settings/production.py
+++ QuizProject/settings/production.py
@@ -2,10 +2,21 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "../db.sqlite3",
+#
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "../db.sqlite3",
-
+        "ENGINE": config("SQL_ENGINE"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
     }
 }
